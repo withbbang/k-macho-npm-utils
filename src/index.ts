6d92e4e46344b4f26faf92a361601c3a2d7576c8
unknown <100463@wpayments.int>
2025-08-28 17:31:13 +0900

    Test: 테스트용 함수 추가

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import { InitLogging1, InitLogging2 } from './log/InitLogging';
+import { test } from './test/test';
 
 (function () {
   InitLogging1();
   InitLogging2();
 })();
 
-export { InitLogging1, InitLogging2 };
+export { InitLogging1, InitLogging2, test };
---
+++ src/test/test.ts
@@ -0,0 +1,7 @@
+/**
+ * Logs the string 'test' to the console.
+ * @public
+ */
+export function test() {
+  console.log('test');
+}
