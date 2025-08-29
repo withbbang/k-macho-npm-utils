a29a4172091ca6eecacfea2a1b82c4d1c0b6920a
상남자 <withbbang@gmail.com>
2025-08-29 11:49:48 +0900

    Fix: 의존성 주입시 ts 파일 못 찾는 에러 수
    - /path/src/index.ts not found.
    - /path/src/log/test.ts not found.
    - Matched by include pattern 'src/**/*' in 'path/node_modules/k-macho-npm-utils/tsconfig.json'

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import pkg from './package.json';
 import { defineConfig } from 'vite';
 import removeConsole from 'vite-plugin-remove-console';
+import dts from 'vite-plugin-dts';
 
 export default defineConfig({
   base: './',
@@ -9,20 +10,16 @@ export default defineConfig({
       removeConsole({
         external: ['warn', 'error'], // 제외할 console 메서드.
       }),
+    dts({
+      outDir: 'dist',
+      insertTypesEntry: true,
+    }),
   ],
   build: {
     minify: 'terser', // 'terser' or 'esbuild'
     lib: {
-      name: pkg.name,
-      fileName: (format) => {
-        if (format === 'umd') {
-          return 'index.js';
-        } else if (format === 'es') {
-          return 'index.esm.js';
-        } else {
-          return 'index.js';
-        }
-      },
+      name: pkg.name.replace(/^@.*\//, '').replace(/[^a-zA-Z0-9_$]/g, ''),
+      fileName: (format) => (format === 'es' ? 'index.esm.js' : 'index.js'),
       entry: 'src/index.ts',
       formats: ['umd', 'es'],
     },
