f2e3d183bf610540477b5cc25604ffb4e1a84c20
상남자 <withbbang@gmail.com>
2025-08-29 16:03:06 +0900

    Chore: dist에 pakcage.json, README.md 제외되도록 수정

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,6 @@ import pkg from './package.json';
 import { defineConfig } from 'vite';
 import removeConsole from 'vite-plugin-remove-console';
 import dts from 'vite-plugin-dts';
-import fs from 'fs';
-import path from 'path';
 
 export default defineConfig({
   base: './',
@@ -16,17 +14,6 @@ export default defineConfig({
       outDir: 'dist',
       insertTypesEntry: true,
     }),
-    {
-      name: 'add license',
-      closeBundle() {
-        // LICENSE를 dist로 복사
-        const src = path.resolve(__dirname, 'LICENSE');
-        const dest = path.resolve(__dirname, 'dist', 'LICENSE');
-        if (fs.existsSync(src)) {
-          fs.copyFileSync(src, dest);
-        }
-      },
-    },
   ],
   build: {
     minify: 'terser', // 'terser' or 'esbuild'
