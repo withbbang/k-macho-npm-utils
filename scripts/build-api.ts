15596176ebb993959bb15872cc4e77fb7ac3abbe
상남자 <withbbang@gmail.com>
2025-08-29 16:14:41 +0900

    Fix: 의존성 주입시 동작 안하는 현상 수정

---
+++ scripts/build-api.ts
@@ -0,0 +1,56 @@
+import { execSync } from 'child_process';
+import { writeFile } from 'fs';
+import { resolve } from 'path';
+import pkg from '../package.json';
+
+function generateApiDeclaration() {
+  execSync('npx tsc --project tsconfig.api.json', { stdio: 'inherit' });
+  execSync(
+    'npx api-extractor run --local --verbose --config ./scripts/api-extractor.json',
+    {
+      stdio: 'inherit',
+    },
+  );
+
+  execSync('rm -rf ./dist/lib', { stdio: 'inherit' });
+}
+
+// 生成 package.json
+function generatePackage() {
+  const {
+    description,
+    types = '',
+    author,
+    license,
+    repository,
+    bugs,
+    homepage,
+    dependencies,
+  } = pkg;
+  const template = {
+    name: pkg.name,
+    version: pkg.version,
+    description,
+    main: pkg.main.replace('dist/', ''),
+    module: pkg.module.replace('dist/', ''),
+    types: types.replace('dist/', ''),
+    declaration: true,
+    author,
+    license,
+    repository,
+    bugs,
+    homepage,
+    dependencies,
+  };
+
+  writeFile(
+    resolve(__dirname, '../dist/package.json'),
+    JSON.stringify(template, null, '\t'),
+    { encoding: 'utf8' },
+    (err) =>
+      console.log(err || `write package.json, current version ${pkg.version}`),
+  );
+}
+
+void generateApiDeclaration();
+void generatePackage();
